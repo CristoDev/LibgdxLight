c4dc9b979dd62d1762932a478adc0676d64b30e7
CristoDev <52540372+CristoDev@users.noreply.github.com>
2019-10-10 16:04:25 +0200

    Modification de la vitesse en fonction des FPS

--- core/src/com/mygdx/game/ContactManager.java
+++ core/src/com/mygdx/game/ContactManager.java
@@ -10,25 +10,19 @@ public class ContactManager implements ContactListener {
     @Override
     public void beginContact(Contact contact) {
         Gdx.app.debug(TAG, "beginContact "+contact.getFixtureA().getBody().getUserData().toString());
-        Fixture b=contact.getFixtureB();
-        //contact.getFixtureB().getBody().setLinearVelocity(new Vector2(0, 0));
-        //contact.getFixtureB().getBody().applyForceToCenter(-10, 0, true);
     }
 
     @Override
     public void endContact(Contact contact) {
         Gdx.app.debug(TAG, "endContact "+contact.getFixtureA().getBody().getUserData().toString());
         Gdx.app.debug(TAG, "*****************************************");
-        //contact.getFixtureB().getBody().setLinearVelocity(new Vector2(0, 0));
     }
 
     @Override
     public void preSolve(Contact contact, Manifold oldManifold) {
-        //Gdx.app.debug(TAG, "preSolve");
     }
 
     @Override
     public void postSolve(Contact contact, ContactImpulse impulse) {
-        //Gdx.app.debug(TAG, "postSolve");
     }
 }
