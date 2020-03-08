9bf8ef5552a95e484588f4afc2926daae458da6a
CristoDev <52540372+CristoDev@users.noreply.github.com>
2020-03-08 15:39:24 +0100

    Mise à jour pour la version HTML

---
+++ android/src/com/light/v1/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.light.v1;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.light.v1.LightGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LightGame(), config);
+	}
+}
