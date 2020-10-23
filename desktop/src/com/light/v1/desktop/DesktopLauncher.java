7001bc2ee190760f5f514362ee688962c8a11da8
cristodev <contact.clefevre@gmail.com>
2020-10-23 16:32:46 +0200

    test git

--- desktop/src/com/light/v1/desktop/DesktopLauncher.java
+++ desktop/src/com/light/v1/desktop/DesktopLauncher.java
@@ -8,6 +8,8 @@ import com.light.v1.LightGame;
 
 
 public class DesktopLauncher {
+	// test du commit avec GIT...
+	private String test="";
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
@@ -16,7 +18,7 @@ public class DesktopLauncher {
 		config.width=1000;
 		config.height=800;
 
-		Gdx.app=new LwjglApplication(new LightGame(), config);;
+		Gdx.app=new LwjglApplication(new LightGame(), config);
 		//Gdx.app.setLogLevel(Application.LOG_INFO);
 		Gdx.app.setLogLevel(Application.LOG_DEBUG);
 		//Gdx.app.setLogLevel(Application.LOG_ERROR);
